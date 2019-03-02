e07900f02cd87d51617d3d67fac005cc8335be80
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2019-03-02 10:42:08 -0300

    Melhorias na geração de dados

--- API-Artigos/database/factories/ArticlesFactory.php
+++ API-Artigos/database/factories/ArticlesFactory.php
@@ -4,15 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Article::class, function (Faker $faker) {
     return [
-      
-      /** Criação de dados faker para minha base de dados
-      * Descriçao e conteuno estao gerando nomes aleatorios para nao dar o erro
-      *  1406 Data too long for column 'descricao'.
-      */
 
         'titulo' => $faker->name,
-        'descricao' => $faker->name,
-        'conteudo' => $faker->name,
+        'descricao' => $faker->text($maxNbChars = 50),
+        'conteudo' => $faker->text($maxNbChars = 200),
         'data' => $faker->dateTime
     ];
 });
